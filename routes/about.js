32b8a96ffbe93aee050f8ca1e31fa72ee74fc6be
Nandika Donthi <nandika@getwellio.com>
2020-03-29 16:43:45 -0700

    added form and post functionality

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('about');
+	res.render('about');
+});
+
+router.post('/', function (req, res) {
+    console.log(req.body.favorite);
+    res.render('index');
 });
 
 module.exports = router;
